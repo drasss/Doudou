e69779ee268b11d1bcaead2a21ec0cb58dfa3d2c
drasss <aurelien640@live.fr>
2025-04-06 14:45:11 +0200

    launch

---
+++ rachel.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import random
+st.set_page_config(
+    page_title="It's ok sweetheart",
+    page_icon="❤️",
+    layout="wide",
+    initial_sidebar_state="expanded"
+    
+)
+
+a,b=st.tabs(["Need love ? ","Need wisdom ? "])
+
+
+def recup(path):
+      fichier=open(path,encoding="UTF-8")
+      dat=fichier.read()
+      fichier.close()
+      dat=dat.split("\n")
+      return dat
+love=recup("love.txt")
+wisdom=recup("Wisdom.txt")
+#                 A   -   Love
+l_text=""
+love_b=a.button("Click here for some love")
+
+if love_b:
+      l_text=love[random.randint(0,len(love))]
+a.text(l_text)
+
+#                 B   -   Wisdom
+w_text=""
+wisdom_b=b.button("Click here for some wisdom")
+
+if wisdom_b:
+      w_text=wisdom[random.randint(0,len(wisdom))]
+b.text(w_text)
+
